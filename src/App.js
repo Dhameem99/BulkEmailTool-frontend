0191cbe5401575bc7507ab1bd3b8147764175262
Dhameem99 <dhameem26@gmail.com>
2023-01-07 13:17:56 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
+import Dashboard from "./Dashboard";
+import User from "./User.js";
+import Login from "./Login";
+import Register from "./Register";
+import Edit from "./Edit"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+      <div id="wrapper">
+        <Sidebar />
+        <div id="content-wrapper" className="d-flex flex-column">
+          <div id="content">
+            <Topbar />
+            <div className="container-fluid">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/user" element={<User />} />
+                <Route path="/edit" element={<Edit />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
