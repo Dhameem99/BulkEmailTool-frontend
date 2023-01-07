4ca4f6681ec0f4d736074496d8e31cf187d9f0d2
Dhameemul Ansari <110928186+Dhameem99@users.noreply.github.com>
2023-01-07 15:27:13 +0530

    Update Register.js

--- src/Register.js
+++ src/Register.js
@@ -9,7 +9,7 @@ function Register() {
  let [email,setEmail]= useState("")
  let [password,setPassword] = useState("")
 
- let url ="http://localhost:4000/bmail/register";
+ let url ="https://bulkemailtool-backend-he8u.onrender.com";
  
  function handleClick(val){
     val.preventDefault();
